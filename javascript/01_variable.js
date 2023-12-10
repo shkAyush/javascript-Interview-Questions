14a08782a87554012006ddc0ea18ab63760f5d61
shkAyush <98070752+shkAyush@users.noreply.github.com>
2023-12-10 04:47:59 +0000

    variable and dataTypes

---
+++ javascript/01_variable.js
@@ -0,0 +1,14 @@
+// A JavaScript variable is simply a name of storage location. There are two types of variables in JavaScript : local variable and global variable.
+
+// There are some rules while declaring a JavaScript variable (also known as identifiers).
+
+// Name must start with a letter (a to z or A to Z), underscore( _ ), or dollar( $ ) sign.
+// After first letter we can use digits (0 to 9), for example value1.
+// JavaScript variables are case sensitive, for example x and X are different variables.
+
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
---
+++ javascript/02_dataTypes.js
@@ -0,0 +1,21 @@
+// JavaScript provides different data types to hold different types of values. There are two types of data types in JavaScript.
+
+// JavaScript has 8 Datatypes
+
+// Primitive data type
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// Non-primitive (reference) data type
+// 1. An object
+// 2. An array
+// 3. A date
+
+
+
